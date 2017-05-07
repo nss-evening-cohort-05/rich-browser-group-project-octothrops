65b812b4833aa648aa15dc503b43d9843970ec12
Isabel Ramos <iramos1226@gmail.com>
2017-05-07 00:07:43 -0500

    getting FbAPI.writeSearchedMovieToDom is not a function error

--- javascripts/movieApi.js
+++ javascripts/movieApi.js
@@ -1,17 +1,25 @@
-var movieAPI = (function (movieCall) {
+var FbAPI = (function (movieCall) {
 
 	movieCall.getMovie = function (searchValue) {
-		return new Promise((resolve, reject) =>{
+		let movies = [];
+		return new Promise((resolve, reject) => {
+			let uid = FbAPI.credentialsCurrentUser().uid;
 			$.ajax({
 				method:"GET",
 				url:`http://www.omdbapi.com/?t=${searchValue}&y=&plot=short&r=json`
-			}).then((response)=>{
-				resolve(response);
-			},(error)=>{
+			}).done((data) => {
+				let response = data;
+				Object.keys(response).forEach((key) => {
+					response[key].id = key;
+					movies.push(response[key]);
+				});
+				resolve(movies);
+			})
+			.fail((error) => {
 				reject(error);
 			});
 		});
 	};
 
 	return movieCall;
-})(movieAPI || {});
+})(FbAPI || {});
